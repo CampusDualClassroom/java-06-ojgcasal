2f0e3678a6b746a365b6aa3b78a50c52b64594e2
Orlando Jose Garces Casal <ojgcasal@gmail.com>
2024-09-27 14:08:01 +0200

    Ejercicio06 resuelto

--- src/main/java/com/campusdual/classroom/Exercise06.java
+++ src/main/java/com/campusdual/classroom/Exercise06.java
@@ -9,6 +9,8 @@ public class Exercise06 {
         Calendar now = Calendar.getInstance();
         int month = now.get(Calendar.MONTH) + 1;
         int hour = now.get(Calendar.HOUR_OF_DAY);
+        checkHour(hour);
+        checkSeason(month);
     }
 
     //TODO ↓
@@ -28,29 +30,30 @@ public class Exercise06 {
                 case 3:
                 case 4:
                 case 5:
-                case 6: {
+                case 6:
+                    System.out.println("It's night");
+                    break;
 
-                }
                 case 7:
                 case 8:
                 case 9:
                 case 10:
                 case 11:
                 case 12:
-                case 13: {
+                case 13:
+                    System.out.println("It's morning");
+                    break;
 
-
-                }
                 case 14:
                 case 15:
                 case 16:
                 case 17:
                 case 18:
                 case 19:
-                case 20: {
+                case 20:
+                    System.out.println("It's afternoon");
+                    break;
 
-
-                }
                 default:
                     throw new IllegalArgumentException("Unexpected value: " + hour);
             }
@@ -66,28 +69,24 @@ public class Exercise06 {
         switch (month) {
             case 1:
             case 2:
-            case 3: {
-
-
-            }
+            case 3:
+                System.out.println("Winter");
+                break;
             case 4:
             case 5:
-            case 6: {
-
-
-            }
+            case 6:
+                System.out.println("Spring");
+                break;
             case 7:
             case 8:
-            case 9: {
-
-
-            }
+            case 9:
+                System.out.println("Summer");
+                break;
             case 10:
             case 11:
-            case 12: {
-
-
-            }
+            case 12:
+                System.out.println("Autumn");
+                break;
             default:
                 throw new IllegalArgumentException("Unexpected value: " + month);
         }
